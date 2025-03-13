5d81e88eee875a7ddd2627988774d62a801e0f6a
Mr. VannyCon <ivan.contrevida4@gmail.com>
2025-03-13 11:58:37 +0800

    1

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'firebasemower',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'firebasemower',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
